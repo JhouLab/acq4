5b6797b1f64fe32bea144737c877dde3981a72be
Thomas Jhou <tomjhou@gmail.com>
2024-12-19 20:41:54 -0500

    Added Configuration selector and JhouLab configurations

---
+++ JhouLabMain.py
@@ -0,0 +1,36 @@
+
+from unittest.mock import patch
+import runpy
+import sys
+
+
+def run_with_args():
+    print('Choose configuration:')
+    print('1. Default (full featured simulation)')
+    print('2. MultiClamp 700B')
+    print('3. Minimal simulation')
+    inp = input('Select option: ')
+    if inp == '1':
+        testargs = ["acq4", "-c", "./config/example/default.cfg"]
+    elif inp == '2':
+        testargs = ["acq4", "-c", "./config/JhouLabConfigs/700B.cfg"]
+    elif inp == '3':
+        testargs = ["acq4", "-c", "./config/JhouLabConfigs/simulated, minimal.cfg"]
+    else:
+        print('Invalid option')
+        return
+    with patch.object(sys, 'argv', testargs):
+        runpy.run_module("acq4", run_name="__main__")
+
+
+USE_CUSTOM_ARGS = True
+
+if __name__ == "__main__":
+    if USE_CUSTOM_ARGS:
+        run_with_args()
+    else:
+        from acq4.__main__ import *
+
+
+#import runpy
+#runpy.run_module("acq4", run_name="__main__");
